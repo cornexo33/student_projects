1ef461e09d7534b3e6599a534e59c90eea50fa3c
cornexo33 <cornexo3@gmail.com>
2024-02-27 13:48:22 +0000

    studentmachine send

--- cpp/cwicenie/main.cpp
+++ cpp/cwicenie/main.cpp
@@ -3,6 +3,26 @@
 using namespace std;
 
 int main(){
-	cout<<"witaj swiecie"<<endl;
-	return 0;
+double odległlośćKM, liczbaOsób, cenaPaliwaZaLitr, spalanie;
+
+cout << "Podaj odleglosc trasy (w km):" ;
+cin >> odległośćKm;
+
+cout << "Podaj ilość osób w aucie: ";
+cin >> liczbaOsób;
+
+cout << "Podaj cene paliwa za litr:";
+cin >> cenaPaliwaZaLitr
+
+cout << "Spalanie na 100km(w litrach): ";
+cin >> spalanie;
+
+double zużyciePaliwa = (odległośćKM / spalanie) * 1.0;
+double kosztCałkowity = zużyciePaliwa * cenaPaliwaZaLitr;
+double kosztNaOsobę = kosztCałkowity / lliczbaOsób;
+
+cout << "Zużycie paliwa: " << zużyciePaliwa << "litrów" << endl;
+cout << "Koszt wyjazdu: " << kosztCałkowity << "zł" << endl;
+cout << "Koszt na osobę" <<kosztNaOsobę << "zł" << endl;
+return 0;
 }
