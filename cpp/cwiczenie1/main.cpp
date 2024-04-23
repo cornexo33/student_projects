ad9b758616c51124f75d3095e226933204919e87
cornexo33 <160610695+cornexo33@users.noreply.github.com>
2024-04-23 14:30:38 +0200

    Update main.cpp

--- cpp/cwiczenie1/main.cpp
+++ cpp/cwiczenie1/main.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 
-float bubbleSort(float x[], int n) {
+float sortowanie(float x[], int n) {
     for (int i = 0; i < n - 1; ++i) {
         for (int j = 0; j < n - i - 1; ++j) {
             if (x[j] > x[j + 1]) {
-                // Zamiana miejscami
+            
                 float temp = x[j];
                 x[j] = x[j + 1];
                 x[j + 1] = temp;
             }
         }
     }
-    // Jeśli liczba elementów jest parzysta, zwróć średnią dwóch środkowych liczb
+    
     if (n % 2 == 0) {
         return (x[n / 2 - 1] + x[n / 2]) / 2.0;
-    } else { // W przeciwnym razie zwróć środkową liczbę
+    } else { 
         return x[n / 2];
     }
 }
@@ -23,7 +23,7 @@ int main() {
     float x[100];
     int n;
 
-    std::cout << "Podaj liczbe elementow (maksymalnie 100): ";
+    std::cout << "Podaj ile liczb (maksymalnie 100): ";
     std::cin >> n;
 
     std::cout << "Podaj " << n << " liczb: ";
@@ -31,16 +31,11 @@ int main() {
         std::cin >> x[i];
     }
 
-    float median = bubbleSort(x, n);
 
 
     std::cout << "Posortowane liczby: ";
     for (int i = 0; i < n; ++i) {
         std::cout << x[i] << " ";
     }
-    std::cout << std::endl;
-
-    std::cout << "Mediana: " << median << std::endl; 
-
     return 0;
 }
